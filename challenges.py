ae831d99681be6e84f70fe03a0fd1e83bead31b8
八木洸太 <yagikota@mbp.local>
2020-09-19 21:15:47 +0900

    "theselftaughtprogrammerchallengesnote"

--- challenges.py
+++ challenges.py
@@ -639,9 +639,58 @@
 
 # mad_libs(text)
 
+# class Stack:
+#     def __init__(self):
+#         self.items = []
+
+#     def is_empty(self):
+#         return self.items == []
+
+#     def push(self, item):
+#         self.items.append(item)
+
+#     def pop(self):
+#         return self.items.pop()
+
+#     def peek(self):
+#         return self.items[-1]
+
+#     def size(self):
+#         return len(self.items)
 
 
+# stack = Stack()
+# stack.size()
+# for c in "yesterday":
+#     stack.push(c)
 
+# reverse = ""
+# while stack.size():
+#     reverse += stack.pop()
+
+# print(reverse)
+
+# stack = Stack()
+
+# for i in range(1, 6):
+#     stack.push(i)
+
+# reverse = []
+
+# for c in range(len(stack.items)):
+#     reverse.append(stack.pop())
+
+# print(reverse)
+
+# for i in range(1, 101):
+#     if i % 15 == 0:
+#         print("FizzBuzz")
+#     elif i % 3 == 0 and i % 15 != 0:
+#         print("Fizz")
+#     elif i % 5 == 0 and i % 15 != 0:
+#         print("Buzz")
+#     else:
+#         print(i) 
 
 
 
